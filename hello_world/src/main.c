60280aa967e1b4ba367028c598f43ca1e31f2475
Adam Łuczak <adam.luczak@dac.digital>
2026-05-19 21:58:51 +0200

    Added custom LED driver for Lecture 6 homework

--- hello_world/src/main.c
+++ hello_world/src/main.c
@@ -5,10 +5,27 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(led_driver0)); 
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+	struct sensor_value value = {0};
+	
+	while(1)
+	{
+		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &value);
+		k_msleep(1000);
+		sensor_sample_fetch(dev);
+		k_msleep(1000);
+	}
 
 	return 0;
 }
